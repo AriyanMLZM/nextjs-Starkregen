9ee0d63ca373cb03800b0d319de91f9e4e495f32
AriyanMlzm <ryanmolazem@gmail.com>
2025-04-05 11:08:30 +0330

    add back btn

--- src/containers/CityPage.tsx
+++ src/containers/CityPage.tsx
@@ -1,8 +1,15 @@
+import Link from 'next/link'
 import { AirQ, Current, ForecastDays, ForecastHours } from './'
+import { IconBack } from '@/icons'
 
 const CityPage = () => {
 	return (
 		<>
+			<div className='w-[1.5rem] fixed top-[80px] left-[10px] bg-black/50 rounded'>
+				<Link href="/">
+					<IconBack className="w-full h-full" />
+				</Link>
+			</div>
 			<Current />
 			<ForecastDays />
 			<ForecastHours />
--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,3 +1,5 @@
 import { FaGithub as IconGithub } from 'react-icons/fa'
 import { LiaSearchLocationSolid as IconSearch } from 'react-icons/lia'
-export { IconGithub, IconSearch }
+import { IoMdArrowRoundBack as IconBack } from 'react-icons/io'
+
+export { IconGithub, IconSearch, IconBack }
