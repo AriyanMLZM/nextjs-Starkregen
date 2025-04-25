98db5aa46d9e6d58cb345e11916b0a2a84c91ff5
AriyanMlzm <ryanmolazem@gmail.com>
2025-04-25 22:20:16 +0330

    fix geolocation

--- src/utils/geoLocation.ts
+++ src/utils/geoLocation.ts
@@ -1,21 +1,23 @@
 const geoLocation: () => Loc | null = () => {
-	if ('geolocation' in navigator) {
-		navigator.geolocation.getCurrentPosition(
-			(position) => {
-				console.log(position.coords)
-				return {
-					lat: position.coords.latitude,
-					lon: position.coords.longitude,
-				}
-			},
-			() => {
-				console.log("Can't access your location!")
-				return null
-			}
-		)
+	if (!navigator.geolocation) {
+		console.log('geolocation not available!')
+		return null
 	}
-	console.log('geolocation not available!')
-	return null
+
+	let loc: Loc | null = null
+	navigator.geolocation.getCurrentPosition(
+		(position) => {
+			console.log(position.coords)
+			loc = {
+				lat: position.coords.latitude,
+				lon: position.coords.longitude,
+			}
+		},
+		(err) => {
+			console.log("Can't access your location!")
+		}
+	)
+	return loc
 }
 
 export default geoLocation
